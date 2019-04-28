6449600da158387256f89901df087f4ff32f359e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-28 21:19:19 +0000

    iunno

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -2,7 +2,9 @@ class CreateSchoolClasses < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
       t.string :title
-      t.string :room_number
-    end 
+      t.integer :room_number
+
+      t.timestamps null: false
+    end
   end
 end
