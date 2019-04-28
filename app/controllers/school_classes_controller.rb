0045a12e93f84a1081088ab1a2352d2116c0f317
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-28 21:37:12 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,10 +13,6 @@ class SchoolClassesController < ApplicationController
     redirect_to post_path(@school_path)
   end
 
-  # @school_class = SchoolClass.new(post_params)
-  #   @school_class.save
-  #   redirect_to school_class_path(@school_class)
-
   def show
     @school_class = SchoolClass.find(params[:id])
   end
