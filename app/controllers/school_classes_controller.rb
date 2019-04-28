f107d6ab8703088c36590b088bb99dacfbc8ef50
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-28 22:28:42 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,6 +8,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.new(post_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -15,8 +18,19 @@ class SchoolClassesController < ApplicationController
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+
+   @school_class.update(post_params)
+   redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def post_params
+    params.require(:school_class).permit(:title, :room_number)
   end
 end
